1196f324506a773dee17f1b499374096fe7e56c1
Martin Camacho <martin@kensho.com>
2017-05-05 20:17:58 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    'app': [
+      'babel-polyfill',
+      'react-hot-loader/patch',
+      './src/index'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.(png|svg)$/, loader: 'file-loader' },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  }
+}
