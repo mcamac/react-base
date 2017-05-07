dc1a469b9c3e2fdf10de9a452a35849a6e05ddb7
Martin Camacho <martin@kensho.com>
2017-05-07 13:24:29 -0400

    Working HMR

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Heh
+          HMR works!
         </p>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App'
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NewRoot = require('./App').default
+    render(NewRoot)
+  })
+}
