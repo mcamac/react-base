be88517974bac80509146bd9b4baa5f3938d075a
Martin Camacho <martin@kensho.com>
2018-04-02 19:41:49 -0400

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react'
 import {hot} from 'react-hot-loader'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">HMR works!</p>
-      </div>
-    )
-  }
-}
+const Home = () => (
+  <div>
+    <div>
+      <h2>Welcome to React</h2>
+    </div>
+    <p>HMR works!</p>
+  </div>
+)
+
+const OtherRoute = () => (
+  <div>
+    <div>
+      <h2>Welcome to React</h2>
+    </div>
+    Other Route
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/test" component={OtherRoute} />
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default hot(module)(App)
