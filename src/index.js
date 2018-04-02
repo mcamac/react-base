59349363b9de330af382c1601fb659c2e13d61f3
Martin Camacho <martin@kensho.com>
2018-04-02 19:37:26 -0400

    Add index.css import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+import './index.css'
+
 const render = Component => {
   ReactDOM.render(<Component />, document.getElementById('root'))
 }
